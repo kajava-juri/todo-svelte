2ff5208a82c467439b87a118186cb71df69f8742
Jüri Kajava <jyri.kajava@tptlive.ee>
2023-03-07 01:51:53 +0200

    onmousedown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -249,7 +249,7 @@
             <p>Description: {todo.description}</p>
             <p>Done: {todo.isDone ? " true" : " false"}</p> 
             <button class="btn btn-danger" on:mousedown={handleDeleteButton(todo.id)}>Delete</button>
-            <button class="btn btn-secondary" on:click={() => handleEditButton(todo)}>Edit</button>
+            <button class="btn btn-secondary" on:mousedown={() => handleEditButton(todo)}>Edit</button>
         </li>
     {/each}
 </ul>
